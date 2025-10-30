1b7b34b2bb30221e2addeec3ed7eaf241c37cfc0
Amir13-11-1383 <156079474+Amir13-11-1383@users.noreply.github.com>
2025-10-30 15:00:52 -0700

    Menu UI rework

--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -1,9 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    public RectTransform Main_Panel;
+    public RectTransform Starting_Game_Panel;
+
     public void StartGameButton()
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene("Prototype");
+        UIAnimationManager.Instance.HideWindow(Main_Panel, 0.5f);
+        UIAnimationManager.Instance.HideWindow(Starting_Game_Panel, 0.5f);
+
+        FindObjectOfType<VideoController>()
+            .PlayVideo(() =>
+            {
+                SceneManager.LoadScene("Prototype");
+            });
     }
 }
