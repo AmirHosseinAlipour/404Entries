cf640ae632a67a6eaaaf75623715b9fd8ac388e8
Amir13-11-1383 <156079474+Amir13-11-1383@users.noreply.github.com>
2025-11-02 17:31:21 -0800

    Commit Before Merging

--- Assets/Scripts/UI/Pause.cs
+++ Assets/Scripts/UI/Pause.cs
@@ -5,8 +5,9 @@ public class Pause : MonoBehaviour
    public RectTransform pausePanel;
    public void OnPause()
    {
-      Time.timeScale = 0;
       UIAnimationManager.Instance.ShowWindow(pausePanel , 0.5f);
+      Time.timeScale = 0;
+      
    }
 
    public void OnUnpause()
